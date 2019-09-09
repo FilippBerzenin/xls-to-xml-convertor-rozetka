c98d1ac5fe2d14d4b84da9010a87c29efe36f362
Filipp Berzenin <filipp.berzenin@gmail.com>
2019-09-09 15:55:15 +0300

    add checker files

--- src/main/java/xlsconvertor/ExcelOperator.java
+++ src/main/java/xlsconvertor/ExcelOperator.java
@@ -1,34 +1,46 @@
 package xlsconvertor;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.TreeMap;
 
 import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.formula.functions.Column;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import lombok.Getter;
 import lombok.extern.java.Log;
 
 @Log
 public class ExcelOperator {
-	
-	private Path pathFoFile;
-	
+
 	@Getter
 	public static Map<String, String> shopProperties;
 	@Getter
@@ -36,7 +48,59 @@ public class ExcelOperator {
 	@Getter
 	public static Map<Integer, Item> offers;
 	private int startColumn = 8;
-	
+
+	public boolean equalsTwoExcelFiles(Path firstPathFoFile, Path secondPathFoFile) {
+		try (Workbook workbookFirst = WorkbookFactory.create(this.createTempFileForExcel(firstPathFoFile).get());
+				Workbook workbookSecond = WorkbookFactory.create(this.createTempFileForExcel(secondPathFoFile).get())) {
+			if (workbookFirst == null || workbookSecond == null) {
+				log.severe("Tomething wrong with WorkbookFactory " + firstPathFoFile + " " + secondPathFoFile);
+				return false;
+			}
+			int artikulNumberForWorkbookFirst = this.findColumnFromName(workbookFirst.getSheet("price"), "Артикул");
+			workbookFirst.getSheet("price").getRow(artikulNumberForWorkbookFirst);
+			int artikulNumberForWorkbookSecond = this.findColumnFromName(workbookSecond.getSheet("price"), "Артикул");
+			workbookFirst.getSheet("price").getRow(artikulNumberForWorkbookSecond);
+			int countDifferentRow = 0;
+			for (int i = 1; i < workbookFirst.getSheet("price").getRow(artikulNumberForWorkbookFirst)
+					.getLastCellNum(); i++) {
+				Cell cell1 = workbookFirst.getSheet("price").getRow(i).getCell(artikulNumberForWorkbookFirst);
+				Cell cell2 = workbookSecond.getSheet("price").getRow(i).getCell(artikulNumberForWorkbookSecond);
+				String s1 = cell1.getStringCellValue();
+				String s2 = cell2.getStringCellValue();
+
+				CellStyle styleForEquals = workbookSecond.createCellStyle();
+				styleForEquals.setFillBackgroundColor(IndexedColors.LIGHT_GREEN.getIndex());
+				styleForEquals.setFillPattern(FillPatternType.THIN_BACKWARD_DIAG);
+
+				CellStyle styleForNoEquals = workbookSecond.createCellStyle();
+				styleForNoEquals.setFillBackgroundColor(IndexedColors.LIGHT_ORANGE.getIndex());
+				styleForNoEquals.setFillPattern(FillPatternType.THIN_BACKWARD_DIAG); // LEAST_DOTS
+
+				if (s1.isEmpty() && s2.isEmpty()) {
+					break;
+				}
+				if (s1.equals(s2)) {
+					workbookSecond.getSheet("price").getRow(i).getCell(artikulNumberForWorkbookFirst)
+							.setCellStyle(styleForEquals);
+				} else {
+					countDifferentRow++;
+					workbookSecond.getSheet("price").getRow(i).getCell(artikulNumberForWorkbookFirst)
+							.setCellStyle(styleForNoEquals);
+					JFrameForArgs.message = countDifferentRow + " positions in the file are tinted - "
+							+ secondPathFoFile;
+				}
+			}
+			try (OutputStream fileOut = new FileOutputStream(secondPathFoFile.toFile())) {
+				workbookSecond.write(fileOut);
+			}
+			return true;
+		} catch (EncryptedDocumentException | InvalidFormatException | IOException e) {
+			e.printStackTrace();
+			JFrameForArgs.message = "Thomething wrong, maybe you don't close file before operation?";
+			return false;
+		}
+	}
+
 	public void readExcelFile(Path pathFoFile) {
 		try (Workbook workbook = WorkbookFactory.create(this.createTempFileForExcel(pathFoFile).get())) {
 			if (workbook == null) {
@@ -50,7 +114,7 @@ public class ExcelOperator {
 					this.setPropertiesForElementsShop(sheet);
 				}
 				if (sheet.getSheetName().equals("category")) {
-					this.setcategoriesShop(sheet);
+					this.setCategoriesShop(sheet);
 				}
 				if (sheet.getSheetName().equals("price")) {
 					this.setOffers(sheet);
@@ -67,7 +131,7 @@ public class ExcelOperator {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private Optional<File> createTempFileForExcel(Path pathFoFile) {
 		File file = null;
 		try {
@@ -79,32 +143,30 @@ public class ExcelOperator {
 		}
 		return Optional.of(file);
 	}
-	
+
 	private void setPropertiesForElementsShop(Sheet sheet) {
 		shopProperties = new HashMap<String, String>();
 		shopProperties.put("name", sheet.getRow(1).getCell(this.findColumnFromName(sheet, "name")).toString());
 		shopProperties.put("company", sheet.getRow(1).getCell(this.findColumnFromName(sheet, "company")).toString());
 		shopProperties.put("url", sheet.getRow(1).getCell(this.findColumnFromName(sheet, "url")).toString());
 		shopProperties.put("currency", sheet.getRow(1).getCell(this.findColumnFromName(sheet, "currency")).toString());
-		JFrameForArgs.message = "Name: "+sheet.getRow(1).getCell(0).toString()+
-				" company: "+sheet.getRow(1).getCell(1).toString()+
-				" url: "+sheet.getRow(1).getCell(2).toString()+
-				" currency: "+sheet.getRow(1).getCell(3).toString()+"\n";
+		JFrameForArgs.message = "Name: " + sheet.getRow(1).getCell(0).toString() + " company: "
+				+ sheet.getRow(1).getCell(1).toString() + " url: " + sheet.getRow(1).getCell(2).toString()
+				+ " currency: " + sheet.getRow(1).getCell(3).toString() + "\n";
 	}
-	
+
 	private int findColumnFromName(Sheet sheet, String nameOfColumn) {
 		int start = sheet.getFirstRowNum();
 		for (Cell cell : sheet.getRow(start)) {
-			if (cell.getCellTypeEnum().equals(CellType.STRING) &&
-				cell.getStringCellValue().equals(nameOfColumn)) {
+			if (cell.getCellTypeEnum().equals(CellType.STRING) && cell.getStringCellValue().equals(nameOfColumn)) {
 				return cell.getColumnIndex();
 			}
 		}
-		JFrameForArgs.message ="Not found Column: "+nameOfColumn;
+		JFrameForArgs.message = "Not found Column: " + nameOfColumn;
 		throw new RuntimeException();
 	}
 
-	private void setcategoriesShop(Sheet sheet) {
+	private void setCategoriesShop(Sheet sheet) {
 		categories = new TreeMap<Integer, String>();
 		int coumtOfRows = this.getLastColumnNum(sheet, 1);
 		for (int i = 1; i < coumtOfRows; i++) {
@@ -113,42 +175,47 @@ public class ExcelOperator {
 			categories.put(in, sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Категория")).toString());
 		}
 	}
-	
+
 	private void setOffers(Sheet sheet) {
 		offers = new TreeMap<Integer, Item>();
 		int countOfRows = getLastColumnNum(sheet, startColumn);
 		int start = 8;
 		for (int i = 1; i < countOfRows; i++) {
 			Cell numberId = sheet.getRow(i).getCell(start);
-			Map<String, String >parameters = new HashMap<>();
+			Map<String, String> parameters = new HashMap<>();
 			for (int k = this.findColumnFromName(sheet, "Артикул"); k < sheet.getRow(0).getLastCellNum(); k++) {
 				String rezult = this.getValueFromCell(sheet.getRow(i).getCell(k));
 				if (rezult.equals("error")) {
 					continue;
 				} else {
-				parameters.put(sheet.getRow(0).getCell(k).getStringCellValue(),
-						this.getValueFromCell(sheet.getRow(i).getCell(k)));
+					parameters.put(sheet.getRow(0).getCell(k).getStringCellValue(),
+							this.getValueFromCell(sheet.getRow(i).getCell(k)));
 				}
 			}
 			int in = (int) numberId.getNumericCellValue();
-			Item item = Item.builder()
-					.ID(in)
+			Item item = Item.builder().ID(in)
 					.available(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Наличие")).getStringCellValue())
-					.price_old(this.getValueFromCell(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Старая цена"))))
+					.price_old(this
+							.getValueFromCell(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Старая цена"))))
 					.price(this.getValueFromCell(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Новая цена"))))
 					.currencyId(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Валюта")).getStringCellValue())
-					.categoryId(this.getValueFromCell(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Категория товара"))))
-					.linksForPicture(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Фото товара")).getStringCellValue().split("\n"))
-					.stock_quantity(this.getValueFromCell(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Количество"))))
+					.categoryId(this.getValueFromCell(
+							sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Категория товара"))))
+					.linksForPicture(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Фото товара"))
+							.getStringCellValue().split("\n"))
+					.stock_quantity(this
+							.getValueFromCell(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Количество"))))
 					.vendor(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Бренд")).getStringCellValue())
-					.name(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Название товара")).getStringCellValue())
-					.description(this.setVaildDescription(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Описание")).getStringCellValue()))
+					.name(sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Название товара"))
+							.getStringCellValue())
+					.description(this.setVaildDescription(
+							sheet.getRow(i).getCell(this.findColumnFromName(sheet, "Описание")).getStringCellValue()))
 					.parameters(parameters).build();
 			offers.put(in, item);
 		}
 		System.out.println("ok");
 	}
-	
+
 	public int getLastColumnNum(Sheet sheet, int numberOfColumn) {
 		int rowCount = 0;
 		Iterator<Row> rows = sheet.iterator();
@@ -165,7 +232,7 @@ public class ExcelOperator {
 		}
 		return rowCount;
 	}
-	
+
 	private String getValueFromCell(Cell num) {
 		String value;
 		if (num.getCellTypeEnum().equals(CellType.STRING)) {
@@ -185,7 +252,7 @@ public class ExcelOperator {
 		}
 		return "error";
 	}
-	
+
 	private String setVaildDescription(String discription) {
 //		String preffix = "<![CDATA[<p>";
 //		String lineSplit = "</p><p>•";
--- src/main/java/xlsconvertor/JFrameForArgs.java
+++ src/main/java/xlsconvertor/JFrameForArgs.java
@@ -3,6 +3,7 @@ package xlsconvertor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -20,56 +21,129 @@ import lombok.extern.java.Log;
 public class JFrameForArgs {
 
 	public static Path pathForExcelFile;
+	public static Path pathForExcelFileForCheck;
 	private XlsToXmlConvertor convertor;
 	public static String message;
+	private boolean selectEqaulsMode = false;
+	private String localMessage;
 
 	public void createGUI() {
 		JFrame f = new JFrame("XLS to XML (Rozetka) konvertor");
 		f.setLocation(450, 250);
 		JLabel lab = new JLabel("Please, enter path for Excel");
 		lab.setBounds(10, 10, 300, 30);
+		
 		JTextField pathToFile = new JTextField(App.localDirectory);
-		pathToFile.setBounds(10, 40, 450, 30);
+		pathToFile.setBounds(10, 40, 360, 30);
 		JButton dir = new JButton("Select...");		
-		dir.setBounds(10, 80, 100, 30);
-		JButton b = new JButton("Transoform");
-		b.setBounds(190, 180, 100, 40);
+		dir.setBounds(375,40, 100, 30);
+
+		JButton equalsSelect = new JButton("Equals mode");		
+		equalsSelect.setBounds(10,80, 120, 30);
+		JTextField pathToFileForEquals = new JTextField(App.localDirectory);
+		pathToFileForEquals.setBounds(10, 80, 360, 30);
+		
+		JButton transform = new JButton("Transoform");
+		transform.setBounds(190, 180, 100, 40);
 		JLabel label1 = new JLabel();
 		label1.setBounds(10, 110, 200, 100);
+		
+		JButton dir2 = new JButton("Select...");		
+		dir2.setBounds(375,80, 100, 30);
+		
 		f.add(lab);
 		f.add(label1);
 		f.add(pathToFile);
 		f.add(dir);
-		f.add(b);
+		f.add(equalsSelect);
+		f.add(transform);
 		f.setSize(500, 300);
 		f.setLayout(null);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		b.addActionListener(new ActionListener() {
+		transform.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				try {
 					pathForExcelFile = Paths.get(pathToFile.getText());
-					log.info(pathForExcelFile.toString());
-//					f.dispose();
-					label1.setText("Args has been submitted.");
-					startClient(pathForExcelFile);
-					label1.setText("Args has been submitted.");
-					JOptionPane.showMessageDialog (null, message, "Message", JOptionPane.INFORMATION_MESSAGE);
-				} catch (RuntimeException e) {
+					if (!selectEqaulsMode && !checkIfExcelFiles(pathForExcelFile)) {
+						localMessage = "This is not Excel file.";
+						label1.setText(localMessage);
+					} else if (!selectEqaulsMode) {
+						log.info(pathForExcelFile.toString());
+						startClient(pathForExcelFile);
+						localMessage = "Args has been submitted.";
+						label1.setText(localMessage);
+						JOptionPane.showMessageDialog (null, message, "Message", JOptionPane.INFORMATION_MESSAGE);
+					}
+					pathForExcelFileForCheck = Paths.get(pathToFileForEquals.getText());
+					if (selectEqaulsMode && 
+							!checkIfExcelFiles(pathForExcelFile) &&
+							!checkIfExcelFiles(pathForExcelFileForCheck)) {
+						label1.setText("This is not Excel file.");
+					} else if (selectEqaulsMode) {
+						startEqualsTwoExcelFiles();
+						label1.setText(localMessage);
+						JOptionPane.showMessageDialog (null, message, "Message", JOptionPane.INFORMATION_MESSAGE);
+					}
+					} catch (RuntimeException e) {
 					JOptionPane.showMessageDialog (null, message, "Message", JOptionPane.INFORMATION_MESSAGE);
 					label1.setText("Args have error values.");
 				}
 			}
 		});
+
 		dir.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				pathToFile.setText(fileChosen());				
+				pathToFile.setText(fileChosen());
 			}
 		});
+		dir2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				pathToFileForEquals.setText(fileChosen());
+			}
+		});
+		equalsSelect.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				equalsSelect.setEnabled(false);
+				f.add(pathToFileForEquals);
+				f.add(dir2);
+				//TODO
+				equalsSelect.disable();
+				equalsSelect.setEnabled(false);
+				selectEqaulsMode = true;
+				f.repaint();
+			}
+		});
+	}
+	
+	private boolean startEqualsTwoExcelFiles () {
+		log.info("File (Excel): " + pathForExcelFile);
+		log.info("File for check (Excel): " + pathForExcelFileForCheck);
+		if (new ExcelOperator().equalsTwoExcelFiles(pathForExcelFile, pathForExcelFileForCheck)) {
+			localMessage ="There are differences in the files.";
+			return true;
+		} else {
+			localMessage = "Not found changes from offers";
+			return false;
+		}
+	}
+	
+	private boolean checkIfExcelFiles (Path xlsFiles) {
+		if (Files.isRegularFile(xlsFiles) && !(
+				xlsFiles.endsWith("xls") ||
+				xlsFiles.endsWith("xlsx"))) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	private String fileChosen() {
